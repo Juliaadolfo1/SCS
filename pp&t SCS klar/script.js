6a89d9377c82c31d9447cb9f7fba31ac56498c1d
Juliaadolfo1 <99642758+Juliaadolfo1@users.noreply.github.com>
2022-12-07 09:49:01 +0100

    Add files via upload

---
+++ pp&t SCS klar/script.js
@@ -0,0 +1,25 @@
+let menu = document.querySelector(".menu")
+let menuItems = document.querySelectorAll(".menuItem")
+let hamburger = document.querySelector(".hamburger")
+let closeIcon = document.querySelector(".closeIcon")
+let menuIcon = document.querySelector(".menuIcon")
+
+function toggleMenu () {
+    if (menu.classList.contains("showMenu")) {
+        menu.classList.remove("showMenu");
+        closeIcon.style.display = "none";
+        menuIcon.style.display = "block";
+    } else {
+        menu.classList.add("showMenu");
+        closeIcon.style.display = "block";
+        menuIcon.style.display = "none";
+    }
+}
+
+hamburger.addEventListener("click", toggleMenu);
+
+menuItems.forEach(
+    function(menuItem) {
+        menuItem.addEventListener("click", toggleMenu)
+    }
+)
